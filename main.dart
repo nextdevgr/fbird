f6394d169fbe6bd53084d1836816b37f83c3fdef
nextdevgr <info@nextdev.gr>
2024-11-17 19:33:00 +0200

    main.dart
    Update for video #2

--- main.dart
+++ main.dart
@@ -27,6 +27,15 @@ class _HomeState extends State<Home> {
   bool gameHasStarted = false;
   Timer? gameTimer;
 
+  List<double> pipeX = [2, 3.5, 5];
+  double pipeWidth = 0.2; //
+  double gap = 0.3; //
+  List<List<double>> pipeHeights = [
+    [0.4, 0.6],
+    [0.3, 0.5],
+    [0.5, 0.4],
+  ];
+
   void jump() {
     setState(() {
       time = 0;
@@ -43,6 +52,15 @@ class _HomeState extends State<Home> {
       setState(() {
         birdY = initialPos - height;
 
+        for (int i = 0; i < pipeX.length; i++) {
+          pipeX[i] -= 0.05;
+
+          if (pipeX[i] < -1.5) {
+            pipeX[i] += 3.5;
+            pipeHeights[i] = _generatePipeHeights();
+          }
+        }
+
         if (birdY < -1) {
           birdY = -1;
         }
@@ -53,6 +71,17 @@ class _HomeState extends State<Home> {
         gameHasStarted = false;
       }
     });
+  }
+
+  List<double> _generatePipeHeights() {
+    double minHeight = 0.2;
+    double maxHeight = 0.6;
+    double bottomPipeHeight =
+        minHeight + (maxHeight - minHeight) * (new DateTime.now().millisecondsSinceEpoch % 1000) / 1000;
+
+    double topPipeHeight = 1.0 - bottomPipeHeight - gap;
+
+    return [bottomPipeHeight, topPipeHeight];
   }
 
   @override
@@ -75,19 +104,35 @@ class _HomeState extends State<Home> {
         body: Column(
           children: [
             Expanded(
-              flex: 2,
+              flex: 3,
               child: Container(
                 color: Colors.blue,
-                child: Center(
-                  child: AnimatedContainer(
-                    alignment: Alignment(0, birdY),
-                    duration: Duration(milliseconds: 0),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.yellow,
+                child: Stack(
+                  children: [
+                    for (int i = 0; i < pipeX.length; i++) ...[
+                      Pipe(
+                        pipeX[i],
+                        pipeHeights[i][0],
+                        pipeWidth,
+                        true,
+                      ),
+                      Pipe(
+                        pipeX[i],
+                        pipeHeights[i][1],
+                        pipeWidth,
+                        false,
+                      ),
+                    ],
+                    AnimatedContainer(
+                      alignment: Alignment(0, birdY),
+                      duration: Duration(milliseconds: 0),
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        color: Colors.yellow,
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ),
@@ -97,6 +142,42 @@ class _HomeState extends State<Home> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Pipe extends StatelessWidget {
+  final double pipeX;
+  final double pipeHeight;
+  final double pipeWidth;
+  final bool isBottom;
+
+  Pipe(this.pipeX, this.pipeHeight, this.pipeWidth, this.isBottom);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment(pipeX, isBottom ? 1 : -1),
+      child: Container(
+        width: MediaQuery.of(context).size.width * pipeWidth,
+        height: MediaQuery.of(context).size.height * pipeHeight / 2,
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              blurRadius: 5,
+              offset: Offset(2, 2),
+            ),
+          ],
+          gradient: LinearGradient(
+            colors: [Colors.green.shade700, Colors.green.shade400],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
       ),
     );
